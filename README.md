67380c60cf1b4c9fc4087fd1943d3b36b3c5e5bb
Wern Jie Lim <lwjdev@gmail.com>
2020-06-28 21:15:07 +0800

    added more scripts, info, and some cleanup

---
+++ README.md
@@ -0,0 +1,8 @@
+# dotfiles
+
+This repository contains dotfiles and some scripts to setup my macOS system.
+
+To have most apps setup up and running, simply execute `setup.sh`, which will automatically install formulae, casks and Mac App Store apps I use.
+
+To install dotfiles and other configuration files, simply run the `configure.sh` script. It'll not override existing files, and only perform a soft link rather than copy the files.
+
---
+++ setup.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+echo "Installing Xcode command line tools if necessary..."
+xcode-select --install >> /dev/null 2>&1
+
+echo "Installing brew if necessary..."
+command -v brew >/dev/null 2>&1 || {
+    echo "Installing brew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+}
+
+echo "Running brew update and brew upgrade..."
+brew update
+brew upgrade
+
+print "Installing cli tools..."
+brew install \
+    bash zsh fish \
+    git wget tmux vim \
+    gcc python python3 node npm \
+    openssl ssh-copy-id telnet nmap \
+    ffmpeg imagemagick cocoapods \
+    neofetch youtube-dl \
+    mas \
+    coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt
+
+print "Installing gui tools..."
+brew cask install \
+    iterm2 \
+    visual-studio-code android-studio \
+    firefox google-chrome microsoft-edge tor-browser \
+    discord mtmr mounty \
+    vnc-viewer vnc-server virtualbox \
+    google-earth-pro
+
+
+print "Installing mac app store apps..."
+mas install 497799835  #Xcode
+mas install 1451685025 #Wireguard
+mas install 1147396723 #WhatsApp
+mas install 1480068668 #FB Messenger
+mas install 747648890  #Telegram
+mas install 863486266  #Sketchbook
+mas install 1470168007 #Vectornator Pro
+mas install 937984704  #Amphetamine
+mas install 462054704  #MS Word
+mas install 462058435  #MS Excel
+mas install 462062816  #MS Powerpoint
+mas install 784801555  #MS OneNote
+
+print "Installing tiling window manager and custom bar..."
+brew install \
+    koekeishiya/formulae/yabai \
+    koekeishiya/formulae/skhd
+brew cask install ubersicht
+
+echo
+echo "Automated installations are done!"
+echo
+echo "To start up yabai, skhd and Übersicht:"
+echo " brew services start yabai "
+echo " brew services start skhd "
+echo " open /Applications/Übersicht.app"
+echo
